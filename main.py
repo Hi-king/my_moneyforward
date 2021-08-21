b1be11c89da036d4b3c044a2805a8945486ecd7d
Hi-king <hikingko1@gmail.com>
2021-08-21 17:50:34 +0900

    :star: download csvs

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+from typing import Dict, List, Any, Union, Hashable
+
 import fire
 import moneyforward.moneyforward
+import dateutil.parser
+import dateutil.relativedelta
+import yaml
 
 
-def main():
-    moneyforward.moneyforward.MoneyForward()
+def main(from_date, to_date):
+    config: Dict = yaml.load(open("config.yaml"), Loader=yaml.SafeLoader)
+
+    mf = moneyforward.moneyforward.MoneyForward(config)
+    from_date = dateutil.parser.parse(from_date)
+    to_date = dateutil.parser.parse(to_date)
+    day = from_date
+    mf.download_csv(year=day.year, month=day.month)
+    while day < to_date:
+        day += dateutil.relativedelta.relativedelta(months=1)
+        mf.download_csv(year=day.year, month=day.month)
 
 
 if __name__ == '__main__':
--- moneyforward/moneyforward.py
+++ moneyforward/moneyforward.py
@@ -3,6 +3,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class MoneyForward:
-    def __init__(self):
+    def __init__(self, config):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver.get('https://google.com')
+        self.driver.get('https://id.moneyforward.com/sign_in/email')
+        mail_form = self.driver.find_element_by_name("mfid_user[email]")
+        mail_form.send_keys(config['user'])
+        mail_form.submit()
+
+        password_form = self.driver.find_element_by_name("mfid_user[password]")
+        password_form.send_keys(config['pass'])
+        password_form.submit()
+
+        self.driver.get("https://moneyforward.com/sign_in")
+        self.driver.find_element_by_class_name('submitBtn').submit()
+
+    def download_csv(self, year, month):
+        self.driver.get(f'https://moneyforward.com/cf/csv?month={month}&year={year}')
